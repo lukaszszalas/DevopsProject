296d22edf86b64dcbf52145c6b110e39507e7a2f
ŁukaszSzałas <lukasz.szalas95@gmail.com>
2025-01-23 21:38:38 +0100

    Add minor changes to main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Witaj w prostym projekcie DevOps!"
+    return "Hello in my project for DEVOPS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
